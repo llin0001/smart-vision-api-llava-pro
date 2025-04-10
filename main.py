2a45c387c4b13ae0e70a53eec2399fea6aa269ce
llin0001 <lianglin74@gmail.com>
2025-04-09 20:50:18 -0700

    main.py, app.add_middleware

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Smart Vision API (LLaVA-Pro)")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://localhost:8000"] if you want to restrict
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
